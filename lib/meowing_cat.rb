5039a59664c4b7cf32d3a2baa263a9b63416bb57
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-27 16:10:57 +0300

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
 ## code your solution here. 
+class Cat
+    attr_accessor : name
+
+    def meow
+        puts "meow!"
+    end
+end
+
+cat1 = Cat.new
+cat1.name = "Maru"
+cat1.sound = "meow"
+puts cat1.sound
